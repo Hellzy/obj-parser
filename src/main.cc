4a0362653091e2c4ea8f5f5d2b4d55e0b65275dd
Francois Mazeau <mazeau_f@epita.fr>
2018-08-12 12:27:35 +0200

    obj-parser: add argument passing to main tester

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,16 @@
+#include <iostream>
+
 #include "parser.hh"
 
-int main()
+int main(int argc, char** argv)
 {
-    OBJParser parser("cube.obj");
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " 'path/to/obj'\n";
+        return 1;
+    }
+
+    OBJParser parser(argv[1]);
 
     auto meshes = parser.meshes_get();
     auto vertices = parser.vertices_get();
